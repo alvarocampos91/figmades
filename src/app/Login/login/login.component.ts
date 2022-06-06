6a2423a504da5df9b52d40f7bf84b4b764895bb5
Alvaro <alvaro@facturatech.co>
2022-06-06 17:46:51 -0500

    Validaciones

--- src/app/Login/login/login.component.ts
+++ src/app/Login/login/login.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-login',
@@ -9,7 +18,25 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
+  statusEye: boolean = true;
+  email: string = '';
+
   ngOnInit(): void {
   }
 
+  toogleEye() {
+    this.statusEye = !this.statusEye;
+  }
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  phoneFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/[\d]{10}/)
+  ]);
+
+  matcher = new MyErrorStateMatcher();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './Login/login/login.component';
+import { LoginComponent, MyErrorStateMatcher } from './Login/login/login.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -13,6 +13,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,8 @@ import {MatButtonModule} from '@angular/material/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatProgressSpinnerModule,
     NoopAnimationsModule,
     MatFormFieldModule,
@@ -32,7 +36,9 @@ import {MatButtonModule} from '@angular/material/button';
     MatInputModule,
     MatIconModule
   ],
-  providers: [],
+  providers: [
+    {provide: ErrorStateMatcher, useClass: MyErrorStateMatcher}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
